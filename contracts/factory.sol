55e372f7b4ebd3d852e78483d5fb7b02a3b16213
JesusRGil <108557695+JRGIL12@users.noreply.github.com>
2025-03-22 05:26:15 -0400

    update SimpleContract to include a phrase and modify createContract to accept it as a parameter

--- contracts/factory.sol
+++ contracts/factory.sol
@@ -3,17 +3,23 @@ pragma solidity ^0.8.0;
 
 contract SimpleContract {
     address public owner;
+    string public phrase;
 
-    constructor(address _owner) {
+    constructor(address _owner, string memory _phrase) {
         owner = _owner;
+        phrase = _phrase;
+    }
+
+    function getPhrase() public view returns (string memory) {
+        return phrase;
     }
 }
 
 contract Factory {
     SimpleContract[] public contracts;
 
-    function createContract() public returns (address) {
-        SimpleContract newContract = new SimpleContract(msg.sender);
+    function createContract(string memory _phrase) public returns (address) {
+        SimpleContract newContract = new SimpleContract(msg.sender, _phrase);
         contracts.push(newContract);
         return address(newContract);
     }
